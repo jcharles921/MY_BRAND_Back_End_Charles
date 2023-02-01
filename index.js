09c1a3613f83e2eda1724bb5ecdb025bb1cfc11a
ykut <ykut@kali.zd>
2023-02-02 01:53:09 +0200

    hambourger side bar , it took hours to do it

--- index.js
+++ index.js
@@ -38,9 +38,22 @@ form.addEventListener("click", function(event) {
     console.log("its submitted")
   }
 })
-function test(){
-    console.log("Hello");
-};
+
 function submit(){
     console.log("its submitted")
 }
+const hamburgerButton = document.querySelector('.hambourger');
+const hamburgerView = document.querySelector('.hamburger_view');
+
+hamburgerButton.addEventListener('click', function() {
+  hamburgerView.style.display = 'flex';
+  hamburgerView.style.visibility = 'visible';
+  hamburgerView.style.position = 'fixed';
+});
+const hamburgerButton_close = document.querySelector('.Flexer_2');
+
+hamburgerButton_close.addEventListener('click', function() {
+  hamburgerView.style.display = 'none';
+  hamburgerView.style.visibility = 'none';
+  hamburgerView.style.position = 'none';
+});
